a91bf894865879a93701488a46ec15923f0d20ef
lesbleu <lesbleu@evercraft.fr>
2016-07-16 03:15:07 +0200

    FLUID_ITEM_STACK

--- src/main/java/fr/evercraft/eversigns/EverSigns.java
+++ src/main/java/fr/evercraft/eversigns/EverSigns.java
@@ -1,3 +1,19 @@
+/*
+ * This file is part of EverSigns.
+ *
+ * EverSigns is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EverSigns is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EverSigns.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.evercraft.eversigns;
 
 import org.spongepowered.api.plugin.Dependency;
@@ -6,10 +22,10 @@ import org.spongepowered.api.plugin.Plugin;
 import fr.evercraft.everapi.plugin.EPlugin;
 import fr.evercraft.eversigns.command.sub.ESReload;
 
-@Plugin(id = "fr.evercraft.everkits", 
-		name = "EverKits", 
+@Plugin(id = "fr.evercraft.eversigns", 
+		name = "EverSigns", 
 		version = "1.2", 
-		description = "Manage Kit",
+		description = "Manage Signs",
 		url = "http://evercraft.fr/",
 		authors = {"rexbut"},
 		dependencies = {
