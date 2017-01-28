98170dc1cf746dcda3b9720c1e6a89b2c91577e1
rexbut <butbutdu45@gmail.com>
2017-01-28 14:42:07 +0100

    Update

--- src/main/java/fr/evercraft/eversigns/command/sub/ESReload.java
+++ src/main/java/fr/evercraft/eversigns/command/sub/ESReload.java
@@ -16,7 +16,7 @@
  */
 package fr.evercraft.eversigns.command.sub;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -47,7 +47,7 @@ public class ESReload extends ESubCommand<EverSigns> {
 	}
 	
 	public Collection<String> subTabCompleter(final CommandSource source, final List<String> args) throws CommandException {
-		return new ArrayList<String>();
+		return Arrays.asList();
 	}
 
 	public Text help(final CommandSource source) {
