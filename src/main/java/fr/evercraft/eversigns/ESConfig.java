84dafe834025def3881952380fd71a855fbaa875
rexbut <butbutdu45@gmail.com>
2017-03-19 22:42:46 +0100

    Update

--- src/main/java/fr/evercraft/eversigns/ESConfig.java
+++ src/main/java/fr/evercraft/eversigns/ESConfig.java
@@ -33,7 +33,7 @@ public class ESConfig extends EConfig<EverSigns> {
 	
 	public void reload() {
 		super.reload();
-		this.plugin.getLogger().setDebug(this.isDebug());
+		this.plugin.getELogger().setDebug(this.isDebug());
 	}
 	
 	@Override
